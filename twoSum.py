0222ee045d8a768e89a57a192d6142e803e22a22
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2026-09-10 12:37:06 +0500

    two sum

--- twoSum.py
+++ twoSum.py
@@ -11,6 +11,6 @@ def two_sum(nums, target):
 
 
 
-print(two_sum([2, 7, 11, 15], 9))   
-print(two_sum([3, 2, 4], 6))        
-print(two_sum([3, 3], 6))           
+print(two_sum([2, 7, 11, 15], 9))
+print(two_sum([3, 2, 4], 6))
+print(two_sum([3, 3], 6))
