213c932797cc06fe78dc8b1a915e8bca07865bcb
Shahzaib Fayyaz <shahzaibdev797@gmail.com>
2026-09-13 11:24:27 +0500

    binary Search

---
+++ Binary Search.py
@@ -0,0 +1,20 @@
+def binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+# --- Example usage ---
+print(binary_search([1, 3, 5, 7, 9, 11, 13], 7))
+print(binary_search([1, 3, 5, 7, 9], 4))
+print(binary_search([2, 4, 6, 8, 10], 10))
